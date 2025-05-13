f372f7f2e4935493410fa6a073dd71765e7eae4a
Ashiq Khan <ashiq@byldd.com>
2025-05-13 16:30:11 +0530

    fix: build error fixed

--- frontend/src/app/galleries/page.tsx
+++ frontend/src/app/galleries/page.tsx
@@ -1,5 +1,10 @@
 import GalleryList from "@/module/galleries/components/GalleryList";
+import { Suspense } from "react";
 
 export default function GalleriesPage() {
-  return <GalleryList />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <GalleryList />
+    </Suspense>
+  );
 }
